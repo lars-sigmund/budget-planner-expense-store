1c7dce2f1d0052c3cf7cac7f75563b8d92e1bc03
Lars Sigmund <larsvsigmund@gmail.com>
2025-01-22 13:41:46 +0100

    dev:
    - added basic spring boot project

---
+++ src/main/java/de/sigmund/budget_planner/expense_store/ExpenseStoreApplication.java
@@ -0,0 +1,13 @@
+package de.sigmund.budget_planner.expense_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sigmund/budget_planner/expense_store/ExpenseStoreApplicationTests.java
@@ -0,0 +1,14 @@
+package de.sigmund.budget_planner.expense_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// tests only if the application starts
+	}
+
+}
